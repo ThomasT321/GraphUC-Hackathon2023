9e428ad27132376dd40e4ed11943474f18a01f7e
SamEdlabad <capt.nemotum@gmail.com>
2023-10-11 22:00:38 -0400

    Basic Server Setup Completed

---
+++ website/__init__.py
@@ -0,0 +1,12 @@
+import imp
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'BestDevs'
+
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
---
+++ website/views.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+# Code for routing goes here
